7fdb1f220a007f90404868fd4b4859cb2f57aacd
jinifief <jinified@gmail.com>
2015-12-11 01:46:55 +0800

    Remove sub and pub list

--- src/rosbridge/configs/baseConfig.py
+++ src/rosbridge/configs/baseConfig.py
@@ -2,45 +2,44 @@
 from collections import namedtuple
 
 import rospy
-from sensor_msgs.msg import Image,CompressedImage
 from geometry_msgs.msg import Vector3Stamped
+from sensor_msgs.msg import CompressedImage, Image
 
-from visionx.msg import depth
+from visionx.msg import *
+from visionx.srv import *
 
-
-SubscribeArg = namedtuple('SubscribeArg', 'topic msg_type')
 
 class BaseConfig(object):
     """
     Basic configuration that stores details pertaining to ROS related parameters and the task
     """
 
-    #Details of system-wide subscribers used
-    system_subscribers = {'heading':SubscribeArg(topic='/navigation/RPY', msg_type=Vector3Stamped),
-                    'depth':SubscribeArg(topic='/depth', msg_type=depth),
-                    'frontcam_compressed':SubscribeArg(topic='/frontcam/camera/image_color/compressed', 
-                        msg_type=CompressedImage),
-                    'frontcam_raw':SubscribeArg(topic='/frontcam/camera/image_color', 
-                        msg_type=Image),
-                    'bottomcam_compressed':SubscribeArg(topic='/bottomcam/camera/image_color/compressed',
-                        msg_type=CompressedImage),
-                    'bottomcam_raw':SubscribeArg(topic='/bottomcam/camera/image_color',
-                        msg_type=Image)}
+    #Topic and message type pair tuple
+    Message = namedtuple('Message', 'topic type')
 
-    #Details of system-wide publishers used
-    system_publishers = {'output':SubscribeArg(topic='/camera_processed/compressed', msg_type=CompressedImage)}
+    #Details of system-wide msgs
+    sys_msgs = {'heading':Message(topic='/navigation/RPY', type=Vector3Stamped), 
+                     'depth':Message(topic='/depth', type=depth),
+                     'frontcam_compressed':Message(topic='/frontcam/camera/image_color/compressed', 
+                         type=CompressedImage),
+                     'frontcam_raw':Message(topic='/frontcam/camera/image_color', 
+                         type=Image),
+                     'bottomcam_compressed':Message(topic='/bottomcam/camera/image_color/compressed', 
+                         type=CompressedImage),
+                     'bottomcam_raw':Message(topic='/bottomcam/camera/image_color', 
+                         type=Image),
+                     'vision_output':Message(topic='/camera_processed/compressed',
+                         type=CompressedImage),
+                     'navigation_server':Message(topic='LocomotionServer', type=ControllerAction), 
+                     'pid':Message(topic='/set_controller_srv', type=set_controller)}
 
-    def __init__(self, name, detector, sub_list=[], pub_list=[]):
+    def __init__(self, name, detector):
         """
         Args:
             name: name of task
-            sub_list: list of subscribers used during task
-            pub_list: list of publishers used during task
         """
         self.name = name
         self.detector = detector
-        self.sub_list = sub_list
-        self.pub_list = pub_list
 
 if __name__ == "__main__":
-    baseConfig = BaseConfig("Test", None)
+    baseConfig = BaseConfig("lane", None)
