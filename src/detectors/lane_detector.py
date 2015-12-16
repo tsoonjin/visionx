cb7337f820788f3d26aadb1d0d9deb9b5e24937a
jinifief <jinified@gmail.com>
2015-12-16 13:54:21 +0800

    Remove base detector

---
+++ src/detectors/lane_detector.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+
+class LaneDetector(object):
+    def __init__(self):
+        print("Init")
+
+if __name__ == "__main__":
+    lane_detector = LaneDetector()
