9ed515e705b926bd0083db09b43fcf88066f694e
jinifief <jinified@gmail.com>
2015-12-18 12:44:11 +0800

    Basic lane structure completed

--- src/taskrunners/lanerunner.py
+++ src/taskrunners/lanerunner.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
+from collections import namedtuple
+
 from detectors.lane_detector import LaneDetector
 from utils import *
 from configs.base_config import BaseConfig
 from rosbridge.base_comm import BaseComm
 
+class Disengage(GenericState):
+    def __init__(self):
+        self.transitions = {'static':'JUST OBSERVING', 'activated':'SEARCHING LANE'}
+        self.sleep_duration = 5
+        super(Disengage, self).__init__('DISENGAGE', self.transitions)
 
-def setup(name):
-    """Setup configuration and communication of ROS node
-    Args:
-        name: name of ROS node
-    Returns:
-        comm: object that handles communication with ROS
-    """
-    config = BaseConfig(name, LaneDetector())
-    rospy.init_node(lane_config.name)
-    comm = BaseComm(lane_config)
-    return comm
+    @start_time
+    def execute(self, userdata):
+        if userdata.static:
+            changeState('static')
+        while isDormant:
+            rospy.sleep(rospy.Duration(self.sleep_duration))
+            changeState('activated')
 
-def executeTask(comm):
-    states_list = 
+class SearchLane(GenericState):
+    def __init__(self):
+        self.transitions = {'detected':'CENTERING TO LANE'}
+        super(Disengage, self).__init__('SEARCHING LANE', self.transitions)
+
+    @start_time
+    def execute(self, userdata):
+        if userdata.static:
+            changeState('static')
+        while isDormant:
+            rospy.sleep(rospy.Duration(self.sleep_duration))
+            changeState('activated')
+
+def setupStates(comm):
+    states_list = [Disengage(), Static(), SearchLane()]
     
-
 if __name__ == "__main__":
-    comm = setup('lane')
-    startTask(
+    config = BaseConfig('lane', LaneDetector())
+    comm = BaseComm(config)
+    states_list = setupStates(comm)
+    startTask(comm, states_list)
