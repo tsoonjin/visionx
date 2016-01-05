b52722d1a8581e17974e39bc2ffc49036ea2ec03
jinifief <jinified@gmail.com>
2016-01-05 12:41:38 +0800

    Merge stats and conversion

--- scripts/vision_lib/core.py
+++ scripts/vision_lib/core.py
@@ -1,6 +1,51 @@
 #!/usr/bin/env python3
+import rospy
 import cv2
 import numpy as np
+
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+bridge = CvBridge()
+
+#====================================
+# Handles conversion of image formats
+#====================================
+
+def readCompressed(rosimg):
+    np_arr = np.fromstring(rosimg.data, np.uint8)
+    return cv2.imdecode(np_arr, cv2.IMREAD_ANYCOLOR)
+
+def writeCompressed(img):
+    msg = CompressedImage()
+    msg.header.stamp = rospy.Time.now()
+    msg.format = "jpeg"
+    msg.data = np.array(cv2.imencode('.jpeg', img)[1]).tostring()
+    return msg
+
+def sk_to_cv(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+def cv_to_sk(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def rosimg2cv(ros_img):
+    try:
+        frame = bridge.imgmsg_to_cv2(ros_img, desired_encoding="bgr8")
+    except CvBridgeError as e:
+        rospy.logerr(e)
+
+    return frame
+
+def cv2rosimg(cv_img):
+    try:
+        return bridge.cv2_to_imgmsg(cv_img, encoding="bgr8")
+    except CvBridgeError as e:
+        rospy.logerr(e)
+
+#=======================================
+# Image statistics and utility functions
+#=======================================
 
 def checkRectangle(cnt,ratio_limit=1.2):
     rect = cv2.minAreaRect(cnt)
